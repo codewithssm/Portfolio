1d60fb30dc5452cae4dec2d536c392d261cfb586
Sahil <sm@gmail.com>
2023-07-22 18:07:14 +0530

    My Personal Portfolio

---
+++ PORTFOLIO/projects/Fitness Hub/script.js
@@ -0,0 +1,101 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
+
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message'),
+      contactUser = document.getElementById('contact-user')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // Check if the field has a value
+    if(contactUser.value === ''){
+        // Add and remove color
+        contactMessage.classList.remove('color-green')
+        contactMessage.classList.add('color-red')
+
+        // Show message
+        contactMessage.textContent = 'You must enter your mail ☝'
+
+        // Remove message three seconds
+        setTimeout(() =>{
+            contactMessage.textContent =''
+        }, 3000)
+    } else{
+        // serviceID
+        emailjs.sendForm('service_4knv6os','template_w3ztplr','#contact-form','fa0Ok9z01UoZKrnq9')
+             .then(() =>{
+                // show message and add color
+                contactMessage.classList.add('color-green')
+                contactMessage.textContent = 'You registered successfully 💪'
+             })
+    }
+}      
+
+contactForm.addEventListerner('submit', sendEmail)
